8227d4dc6b6ee66f2ef811ea96ccb866dc246285
Justine <justinesarmiento906@gmail.com>
2025-02-01 21:14:50 +0800

    message to

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    <p>Hellowww</p>
-    <h1>mother father</h1>
-    <h2>beaches</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui fugiat facere ipsum numquam similique repudiandae magni dolores a quibusdam odio est assumenda deserunt error, quisquam laborum ea officia, veritatis nihil?</p>
 </body>
 </html>
